a4b095e9d3eae87c08de668ba90cff4d7a477f94
fenix-125 <yurijpasichnyk11@gmail.com>
2020-01-12 16:39:11 +0200

    synchronize data files

--- scripts/pars_relig.py
+++ scripts/pars_relig.py
@@ -1,6 +1,17 @@
-with open("demo_data/national_religions.csv") as f:
-    with open("data/religion_2015.csv", "w+") as f_out:
-        print(f.readline(), file=f_out, end="")
-        for line in f:
-            if line.startswith("2010,"):
-                print(line[5:], file=f_out, end="")
+with open("../data_demo/happiness_2015.csv") as f_r:
+    with open("../data_demo/religion_2015.csv") as f_h:
+        with open("../data/religion.csv", "w+") as f_r_out:
+            with open("../data/happiness.csv", "w+") as f_h_out:
+                print(f_r.readline(), file=f_r_out, end="")
+                print(f_h.readline(), file=f_h_out, end="")
+                try:
+                    for line_r in f_r:
+                        f_h.seek(0)
+                        f_h.readline()
+                        for line_h in f_h:
+                            if line_r.split(",")[0] == line_h.split(",")[0]:
+                                print(line_r, file=f_r_out, end="")
+                                print(line_h, file=f_h_out, end="")
+                                break
+                except EOFError:
+                    pass
